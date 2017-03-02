1196c5f27303329502fde5a26714d5cc9bdbf6e5
JK <jinhh999@gmail.com>
2017-03-02 21:41:24 +0800

    抽取公共模块

--- app/app.js
+++ app/app.js
@@ -3,12 +3,29 @@
 // Declare app level module which depends on views, and components
 var app = angular.module('myApp', [
 	'ngRoute',
-	'app.in_theaters',
-	'app.coming_soon',
-	'app.top250'
+	'app.movie_list'
 
 ]);
 app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+	//去除html5造成的地址栏中#后自动添加前缀乱码
 	$locationProvider.hashPrefix('');
 	$routeProvider.otherwise({redirectTo: '/in_theaters/1'});
-}]);
+}])
+ .controller('appController', [
+   '$scope',
+   '$location',
+   function($scope, $location) {
+     $scope.$location = $location;
+     $scope.$watch('$location.path()', function(now) {
+       if (now.startsWith('/in_theaters')) {
+         $scope.type = 'in_theaters';
+       } else if (now.startsWith('/coming_soon')) {
+         $scope.type = 'coming_soon';
+       } else if (now.startsWith('/top250')) {
+         $scope.type = 'top250';
+       }
+       console.log($scope.type);
+     });
+   }
+ ])
+;
--- app/movie_list/controller.js
+++ app/movie_list/controller.js
@@ -4,25 +4,25 @@
 (function (angular) {
 	'use strict';
 	//创建了正在上映的模块
-	var module = angular.module('app.in_theaters', ['ngRoute', 'app.services.jsonp']);
+	var module = angular.module('app.movie_list', ['ngRoute', 'app.services.jsonp']);
 
 	//配置了单独的路由
 	module.config(['$routeProvider', function ($routeProvider) {
 		$routeProvider
-			.when('/in_theaters/:page', {
-				templateUrl: 'in_theaters/view.html',
-				controller: 'inTheatersController'
+			.when('/:category/:page', {
+				templateUrl: 'movie_list/view.html',
+				controller: 'MovieListController'
 			})
 	}]);
 	//配置了单独的控制器
-	module.controller('inTheatersController', [
+	module.controller('MovieListController', [
 		'$scope', '$route', '$routeParams', 'jsonp',
 		function ($scope, $route, $routeParams, jsonp) {
 			//console.log($routeParams.page);
 			$scope.loading = true;
 			$scope.title = "";
 			//设置默认的单页显示信息数
-			var count = 5;
+			var count = 10;
 			//获取当前的地址栏的页数
 			var page = parseInt($routeParams.page);
 			//计算当前页展示的第一条信息的位置
@@ -33,9 +33,11 @@
 			$scope.totalPages = 0;
 			//当前的页数
 			$scope.currentPage = page;
+			//
+			$scope.currentCategory  = $routeParams.category;
 			$scope.subjects = [];
 			jsonp.json(
-				'https://api.douban.com/v2/movie/in_theaters',
+				'https://api.douban.com/v2/movie/'+$routeParams.category,
 				{count: count, start: start},
 				function (data) {
 					//console.log(data);
@@ -52,7 +54,10 @@
 						//限定可被点击的范围[1，totalPages]
 						if (page >= 1 && page <= $scope.totalPages)
 							$route.updateParams({ page: page });
-					}
+					};
+					/*
+					console.log($scope.currentCategory);
+					console.log($scope.currentCategory=='in_theaters');*/
 				}
 			)
 		}]);
